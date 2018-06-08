8b5aaa4df06512c3f84c00f9a3b8ab40cef526e3
TOSHIUBA\Brenda <brendamorpe@gmail.com>
2018-06-07 23:12:38 -0500

    Function Descifrar y CSS

--- src/cipher.js
+++ src/cipher.js
@@ -7,7 +7,7 @@ salt = document.getElementById("num_salt");
 function cifrar()
       {
 
-
+let result ="";
 let mensa = men.value;
 let saltos = parseInt(salt.value);
 for (i = 0; i < mensa.length; i++) {
@@ -15,49 +15,52 @@ let no_ascii = mensa.charCodeAt(i);
 
 if (no_ascii >= 65 && no_ascii <= 90){
 
-result = String.fromCharCode((no_ascii - 65 + saltos) % 26 + 65);
+result += String.fromCharCode((no_ascii - 65 + saltos) % 26 + 65);
 document.getElementById("resultado").innerHTML = result;
 
 }else if (no_ascii >= 97 && no_ascii <= 122) {
- result =  String.fromCharCode((no_ascii - 97 + saltos) % 26 + 97);
+ result +=  String.fromCharCode((no_ascii - 97 + saltos) % 26 + 97);
 document.getElementById("resultado").innerHTML = result;
 console.log(result);
 }else
 
 if (no_ascii === 32) {
- result = String.fromCharCode(no_ascii);
+ result += String.fromCharCode(no_ascii);
+
    document.getElementById("resultado").innerHTML = result;
 console.log(result);
  }
 }
      }
 
+
+
      function descifrar()
-           {
+          {
 
+let descifrado ="";
+    let mensa = men.value;
+    let saltos = parseInt(salt.value);
+    for (i = 0; i < mensa.length; i++) {
+    let no_ascii = mensa.charCodeAt(i);
 
-     let mensa = men.value;
-     let saltos = parseInt(salt.value);
-     for (i = 0; i < mensa.length; i++) {
-     let no_ascii = mensa.charCodeAt(i);
+    if (no_ascii >= 65 && no_ascii <= 90){
 
-     if (no_ascii >= 65 && no_ascii <= 90){
+   descifrado += String.fromCharCode((no_ascii - 65 - saltos) % 26 + 65);
+    document.getElementById("resultado").innerHTML = descifrado;
 
-    let descifrado = String.fromCharCode((no_ascii - 65 - saltos + 26 * 2) % 26 + 65);
+    }else if (no_ascii >= 97 && no_ascii <= 122) {
+    descifrado +=  String.fromCharCode((no_ascii - 97 -saltos) % 26 + 97);
+    document.getElementById("resultado").innerHTML = descifrado;
+    console.log(descifrado);
+  }else
+
+  if (no_ascii === 32) {
+   descifrado += String.fromCharCode(no_ascii);
      document.getElementById("resultado").innerHTML = descifrado;
+  console.log(descifrado);
+   }
 
-     }else if (no_ascii >= 97 && no_ascii <= 122) {
-     let descifrado =  String.fromCharCode((no_ascii - 97 -saltos + 26 * 2) % 26 + 97);
-     document.getElementById("resultado").innerHTML = descifrado;
-     console.log(descifrado);
-   }else
 
-   if (no_ascii === 32) {
-    descifrado = String.fromCharCode(no_ascii);
-      document.getElementById("resultado").innerHTML = descifrado;
-   console.log(descifrado);
     }
-
-
-     }
-          }
+         }
