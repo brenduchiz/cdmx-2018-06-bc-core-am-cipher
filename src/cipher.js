bce67fef86aaff26b7c6f2f696d8c9a035018f7d
TOSHIUBA\Brenda <brendamorpe@gmail.com>
2018-06-13 18:46:53 -0500

    Run with Object Window.Cypher

--- src/cipher.js
+++ src/cipher.js
@@ -1,68 +1,63 @@
 window.cipher = {
-  // ...
-};
-men = document.getElementById("mensaje");
-salt = document.getElementById("num_salt");
 
-function cifrar()
-      {
+
+
+encode:(offset,string) => {
 
 let result ="";
-let mensa = men.value;
-let saltos = parseInt(salt.value);
-for (i = 0; i < mensa.length; i++) {
-let no_ascii = mensa.charCodeAt(i);
+
+for (i = 0; i < string.length; i++) {
+
+let no_ascii = string.charCodeAt(i);
 
 if (no_ascii >= 65 && no_ascii <= 90){
 
-result += String.fromCharCode((no_ascii - 65 + saltos) % 26 + 65);
+result += String.fromCharCode((no_ascii - 65 +  parseInt(offset)) % 26 + 65);
+
 document.getElementById("resultado").innerHTML = result;
 
 }else if (no_ascii >= 97 && no_ascii <= 122) {
- result +=  String.fromCharCode((no_ascii - 97 + saltos) % 26 + 97);
+ result +=  String.fromCharCode((no_ascii - 97 + parseInt(offset)) % 26 + 97);
 document.getElementById("resultado").innerHTML = result;
 console.log(result);
-}else
-
-if (no_ascii === 32) {
+}else if (no_ascii === 32) {
  result += String.fromCharCode(no_ascii);
 
-   document.getElementById("resultado").innerHTML = result;
-console.log(result);
- }
+ document.getElementById("resultado").innerHTML = result;
+
 }
-     }
+}
 
+},
 
-
-     function descifrar()
-          {
+decode:(offset,string) => {
 
 let descifrado ="";
-    let mensa = men.value;
-    let saltos = parseInt(salt.value);
 
 
-    for (i = 0; i < mensa.length; i++) {
-    let no_ascii = mensa.charCodeAt(i);
+
+    for (i = 0; i <string.length; i++) {
+    let no_ascii = string.charCodeAt(i);
 
     if (no_ascii >= 65 && no_ascii <= 90){
 
-   descifrado += String.fromCharCode((no_ascii - 90 - saltos ) % 26 + 90);
+   descifrado += String.fromCharCode((no_ascii - 90 - parseInt(offset)) % 26 + 90);
     document.getElementById("resultado").innerHTML = descifrado;
 
     }else if (no_ascii >= 97 && no_ascii <= 122) {
-    descifrado +=  String.fromCharCode((no_ascii - 122 - saltos) % 26 + 122);
-    document.getElementById("resultado").innerHTML = descifrado;
+    descifrado +=  String.fromCharCode((no_ascii - 122 - parseInt(offset)) % 26 + 122);
+   document.getElementById("resultado").innerHTML = descifrado;
     console.log(descifrado);
-  }else
-
-  if (no_ascii === 32) {
+  }else if (no_ascii === 32) {
    descifrado += String.fromCharCode(no_ascii);
-     document.getElementById("resultado").innerHTML = descifrado;
-  console.log(descifrado);
+    document.getElementById("resultado").innerHTML = descifrado;
+
    }
 
 
     }
-         }
+
+  }
+
+
+ };
--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
+let string = document.getElementById("mensaje");
+let offset = document.getElementById("num_salt");
+let buttonCifrado=document.getElementById("buttonC");
+let buttonDescifrar=document.getElementById("buttonD");
 
+
+buttonCifrado.addEventListener("click", evento => {window.cipher.encode( offset.value, string.value)});
+buttonDescifrar.addEventListener("click", evento => {window.cipher.decode( offset.value, string.value)});
